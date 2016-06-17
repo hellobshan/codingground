88999645bd117b63213b61d241b2a67ffbb0943a
hellobshan <shankar.bodduluri@gmail.com>
2016-06-17 06:54:19 +0000

    Created by Coding Ground Client

---
+++ Workflow/HelloWorld.java
@@ -0,0 +1,11 @@
+import java.util.UUID;
+
+
+public class HelloWorld {
+	public static void main(String[] args) {
+	    for (int i = 0; i < 15; i++) {
+			System.out.println(UUID.randomUUID());
+		}
+		
+	}
+}
